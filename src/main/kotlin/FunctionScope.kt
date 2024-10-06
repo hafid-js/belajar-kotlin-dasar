4258012b461436606bc72dd6cffc370046778e1d
hafidjs <afidzpratama@gmail.com>
2024-10-06 19:45:16 +0700

    Function scope

---
+++ src/main/kotlin/FunctionScope.kt
@@ -0,0 +1,9 @@
+fun main() {
+    fun contohHelloWorld() {
+        println("Hello World")
+    }
+
+    contohHelloWorld()
+
+    // function contohHelloWorld hanya bisa diakses didalam scope function main, diluar itu tidak bisa
+}
