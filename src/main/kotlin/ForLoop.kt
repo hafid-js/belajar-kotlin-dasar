cc029c4e4579b792de6ac152c27629c9878c3784
hafidjs <afidzpratama@gmail.com>
2024-10-06 14:47:02 +0700

    For Loop

---
+++ src/main/kotlin/ForLoop.kt
@@ -0,0 +1,27 @@
+fun main() {
+
+    var array = arrayOf("Eko", "Kurniawan", "Khannedy", "Joko", "Budi", "Rudi")
+
+    var total = 0
+    for (name in array) {
+        println(name)
+        total++
+    }
+
+    println("Total Perulangan = $total")
+
+    for (i in 0..100) {
+        println(i)
+    }
+
+     for (i in 100 downTo 0 step 2) {
+         println(i)
+     }
+
+    val ukuranArray = array.size - 1
+    for (i in 0..ukuranArray) {
+        println("Index $i = ${array.get(i)}")
+    }
+
+
+}
