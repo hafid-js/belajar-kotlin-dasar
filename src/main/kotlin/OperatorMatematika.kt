79efe86ebd6a2e3018f22c1891ec0eb5b6758966
hafidjs <afidzpratama@gmail.com>
2024-10-06 11:10:11 +0700

    operasi matematika

---
+++ src/main/kotlin/OperatorMatematika.kt
@@ -0,0 +1,30 @@
+fun main() {
+
+    val result = 1.0 / 3.0
+    println(result)
+
+    val result2 = 10 + 10 / 2
+    println(result2)
+
+    var total: Int = 0
+
+    val barang1 = 100
+    total = total + barang1
+
+    val barang2 = 200
+    total = total + barang2
+
+    val barang3 = 300
+    total = total + barang3
+
+    println(total)
+
+    total++
+    println(total)
+
+    val suhu = -5
+    println(suhu)
+
+    val sehat = true
+    println(!sehat)
+}
