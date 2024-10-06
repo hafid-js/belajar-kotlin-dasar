c94aa7263235c0aca5385d04c82e90a75b74ad1b
hafidjs <afidzpratama@gmail.com>
2024-10-06 17:01:05 +0700

    Function Default Paramater

---
+++ src/main/kotlin/FunctionDefaultParameter.kt
@@ -0,0 +1,15 @@
+fun hello (firstName: String, lastName: String? = null){
+    if (lastName == null) {
+        println("Hello $firstName")
+    } else {
+        println("Hello $firstName $lastName")
+    }
+}
+
+fun main() {
+
+    hello("Eko")
+    hello("Eko", "Kurniawan")
+    hello("Budi")
+    hello("Joko", "Nugroho")
+}
