75ff005a56d010a51e78a4edbc31462b9792a2d3
hafidjs <afidzpratama@gmail.com>
2024-10-06 08:12:56 +0700

    tipe data character

--- src/main/kotlin/Character.kt
+++ src/main/kotlin/Character.kt
@@ -1,10 +1,10 @@
 fun main() {
 
-    var char1: Char = 'E'
-    var char2: Char = 'K'
-    var char3: Char = 'O'
+    var e: Char = 'E'
+    var k: Char = 'K'
+    var o: Char = 'O'
 
-    println(char1)
-    println(char2)
-    println(char3)
+    println(e)
+    println(k)
+    println(o)
 }
