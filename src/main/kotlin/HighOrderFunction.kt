06b2ff6abe2aa2bea163b1fd6fcc88e97b869912
hafidjs <afidzpratama@gmail.com>
2024-10-08 06:42:09 +0700

    High Order Function

---
+++ src/main/kotlin/HighOrderFunction.kt
@@ -0,0 +1,18 @@
+fun main() {
+
+    fun hello(name: String, transformer: (String) -> String): String {
+        val nameTransform = transformer(name)
+        return "Hello $nameTransform"
+    }
+
+    val lambdaUpper = {
+        value: String -> value.toUpperCase()
+    }
+    println(hello("Eko", lambdaUpper))
+
+    val result1 = hello("Eko", {
+        value: String -> value.toLowerCase()})
+
+    val result2 = hello("Eko") {
+        value: String -> value.toLowerCase()}
+}
