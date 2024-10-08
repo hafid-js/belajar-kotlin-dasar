57d3486f2b356b547c8ba1b1416f6a56873deab1
hafidjs <afidzpratama@gmail.com>
2024-10-09 06:13:20 +0700

    Komentar

---
+++ src/main/kotlin/Komentar.kt
@@ -0,0 +1,21 @@
+/**
+ * function untuk contoh
+ */
+fun contohFunction() {
+
+}
+
+/**
+ * contoh function dengan parameter
+ * @param name nama parameter
+ */
+fun contohFunctionParamater(name: String) {
+
+}
+
+fun main() {
+
+    // memanggil contoh function
+    contohFunction()
+    contohFunctionParamater("EKo") // memanggil contoh function parameter
+}
